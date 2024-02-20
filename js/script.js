153c050e550e37ce34aa45a802d28d729578b124
Gustavo Freitas <gustavo.mautone.freitas@gmail.com>
2024-02-20 13:52:11 -0300

    First files

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$( '#multiple-select-clear-field' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field2' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field3' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field4' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field5' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field6' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field7' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+$( '#multiple-select-clear-field8' ).select2( {
+  closeOnSelect: true,
+  allowClear: true,
+} );
+
+if(screen.width <= 991) {
+  $('.journey-slick').slick({
+    arrows: false
+  });
+}
